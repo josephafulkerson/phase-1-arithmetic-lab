e65e6c716c60a030701670b64d65a9ea9eb1b7b2
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-08-15 22:38:47 -0400

    commit tree bcc5b4065ed213205ca8d6344587f528aee72b17

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
+const num3 = 10;
+const num4 = 6;
 
-// Write your code here
+const multiply = num1 * num2
+
+console.log(multiply)
+
+const random = Math.ceil(Math.random()* 100);
+console.log(random)
+
+const mod = num3 % num4;
+console.log(mod)
+
+const max = Math.max(1,5,20);
+console.log(max)
